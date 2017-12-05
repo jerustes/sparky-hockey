dfb06fcf75e1f20325f4fd1304d2e051667c67cf
0330230 <0330230@dit161>
2017-12-05 10:57:12 +0100

    Update

--- src/basketball/BasketballPoints.java
+++ src/basketball/BasketballPoints.java
@@ -3,16 +3,16 @@ package basketball;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
-import scala.Tuple2;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Function2;
+import scala.Tuple2;
 
 public class BasketballPoints {
 
